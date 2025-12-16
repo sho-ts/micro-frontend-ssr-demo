726b1b896129f5acd4065d843693d586b397e207
sho <sho.webfe@gmail.com>
2025-12-17 03:42:05 +0900

    fix: 不要なコードを削除

--- apps/fragment/src/app.tsx
+++ apps/fragment/src/app.tsx
@@ -1,10 +1,8 @@
 import { Hono } from 'hono';
 import { AppHeaderRenderer } from '@/src/components/AppHeader/server';
-import { cors } from 'hono/cors'
+import { cors } from 'hono/cors';
 
 export const app = new Hono();
 
 app.use('*', cors());
 app.route('AppHeader', AppHeaderRenderer);
-
-export default app;
