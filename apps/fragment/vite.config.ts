8a118143cb2aa41abf1f64038e07fda470502341
sho <sho.webfe@gmail.com>
2025-12-17 02:48:11 +0900

    add: clientのビルド処理を追加

---
+++ apps/fragment/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname),
+    },
+  },
+  build: {
+    outDir: 'static/client',
+    lib: {
+      entry: path.resolve(__dirname, 'src/entries/client.ts'),
+      formats: ['es'],
+      fileName: () => 'main.js',
+    },
+  },
+});
