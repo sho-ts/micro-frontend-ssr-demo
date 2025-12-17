20889f8a35e7a19049577b4039c9c62f1fddcba7
sho <sho.webfe@gmail.com>
2025-12-18 05:47:01 +0900

    update: 共通処理を移動

--- apps/mpa/app/Providers/AppServiceProvider.php
+++ apps/mpa/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            $headerProps = [
+                'name' => 'John Doe',
+            ];
+            $headerResponse = Http::get('http://host.docker.internal:5174/AppHeader', $headerProps);
+            $header = [
+                'html' => $headerResponse->body(),
+                'props' => json_encode($headerProps),
+            ];
+
+            $view->with('header', $header);
+        });
     }
 }
--- apps/mpa/routes/web.php
+++ apps/mpa/routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $headerProps = [
-        'name' => 'dummy'
-    ];
-    $headerResponse = Http::get('http://host.docker.internal:5174/AppHeader', $headerProps);
-    $header = [
-        'html' => $headerResponse->body(),
-        'props' => json_encode($headerProps),
-    ];
-
-    return view('welcome')->with(
-        'header',
-        $header
-    );
+    return view('welcome');
 });
