24a22d25bd0df5b857e41581d8d8918e5b8bdef2
sho <sho.webfe@gmail.com>
2026-01-06 04:35:29 +0900

    update: SSR処理を共通関数に分離

--- apps/fragment/src/app.tsx
+++ apps/fragment/src/app.tsx
@@ -1,8 +1,12 @@
 import { Hono } from 'hono';
-import { AppHeaderRenderer } from '@/src/components/AppHeader/server';
 import { cors } from 'hono/cors';
+import { AppHeader, AppHeaderSchema } from './components/AppHeader';
+import { defineServerSideComponent } from './ssr';
 
 export const app = new Hono();
 
 app.use('*', cors());
-app.route('AppHeader', AppHeaderRenderer);
+
+const ssrComponents = [AppHeader];
+
+ssrComponents.forEach((component) => defineServerSideComponent(app, component, AppHeaderSchema));
---
+++ apps/fragment/src/ssr.tsx
@@ -0,0 +1,27 @@
+import { Hono } from 'hono';
+import { validator } from 'hono/validator';
+import { renderToString } from 'react-dom/server';
+import z from 'zod';
+
+export const defineServerSideComponent = <T extends z.ZodObject<Record<string, z.ZodType>>>(
+  router: Hono,
+  Component: (props: z.infer<T>) => React.ReactNode,
+  schema?: T
+) => {
+  router.get(
+    `/${Component.name}`,
+    validator('query', (value, c) => {
+      const result = schema?.safeParse(value);
+
+      if (result?.error) return c.json({ message: 'Invalid query parameters' }, 400);
+
+      return result?.data;
+    }),
+    (c) => {
+      const props = c.req.valid('query') as z.infer<T>;
+      const html = renderToString(<Component {...props} />);
+
+      return c.html(html);
+    }
+  );
+};
