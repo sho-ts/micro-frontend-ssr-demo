9acd21720cfe06a43cc6ca442f697fc7df51172a
sho <sho.webfe@gmail.com>
2026-01-07 04:01:43 +0900

    update: versionを返せるようにしておく

--- apps/fragment/src/app.tsx
+++ apps/fragment/src/app.tsx
@@ -2,10 +2,25 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 import { AppHeader, AppHeaderSchema } from './components/AppHeader';
 import { defineServerSideComponent } from './ssr';
+import packageJson from '../package.json';
+
+const version = packageJson.version ?? 'unknown';
+
+if (version === 'unknown') {
+  console.warn(
+    '[warning] The version of fragment app is unknown. Please make sure that the version is correctly set in package.json.'
+  );
+}
 
 export const app = new Hono();
 
 app.use('*', cors());
+
+app.get('/meta', (c) => {
+  return c.json({
+    version,
+  });
+});
 
 const ssrComponents = [
   {
